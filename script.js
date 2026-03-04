a669fb3b855b00b27ef79fc1e14554fdc92e03d8
CDE <carlostradingfxft@gmail.com>
2026-03-04 00:44:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+window.addEventListener('scroll', function() {
+    const nav = document.getElementById('navbar');
+    if (window.scrollY > 30) {
+        nav.style.padding = "10px 5%";
+        nav.style.background = "rgba(10, 14, 23, 0.98)";
+    } else {
+        nav.style.padding = "20px 5%";
+        nav.style.background = "rgba(10, 14, 23, 0.8)";
+    }
+});
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        if (elementTop < windowHeight - 50) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+document.querySelectorAll('.card').forEach(card => {
+    card.style.cursor = "pointer";
+    card.addEventListener('click', () => {
+        const planesSection = document.getElementById('planes');
+        if(planesSection) {
+            planesSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
